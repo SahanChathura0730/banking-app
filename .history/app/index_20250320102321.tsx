a03d3f4b59393b5567faad99814eb10234c5e1d9
Sahan Chathura <sahanchathura0730@gmail.com>
2025-03-20 10:43:14 +0530

    setup the tailwind

---
+++ .history/app/index_20250320102321.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import '../global.css'
+
+const index = () => {
+  return (
+    <View>
+      <Text className="flex-1 items-center justify-center bg-blue-500">index</Text>
+    </View>
+  )
+}
+
+export default index
---
+++ .history/app/index_20250320102414.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import '../global.css'
+
+const index = () => {
+  return (
+    <View>
+      <Text className='text-white text-lg font-bold'>index</Text>
+    </View>
+  )
+}
+
+export default index
